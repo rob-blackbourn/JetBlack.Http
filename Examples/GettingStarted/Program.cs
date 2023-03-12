49c1275098450b93186f6a3674c84b0efb22d8d2
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-03-12 07:43:18 +0100

    Added GettingStarted

---
+++ Examples/GettingStarted/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using JetBlack.Http.Core;
+using JetBlack.Http.Rest;
+
+namespace Example
+{
+    using RestRequest = HttpRequest<RestRouteInfo, RestServerInfo>;
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            }))
+            {
+                var server = new RestServer(loggerFactory)
+                    .AddPrefix("http://*:8081/")
+                    .ConfigureRouter(router => router.IgnoreCase = true)
+                    .AddRoute(SayHello, "/api/v1/helloWorld", "GET")
+                    .AddRoute(SayWithQueryString, "/api/v1/hello")
+                    .AddRoute(SayName, "/api/v1/hello/{name:string}", "GET", "POST")
+                    .AddRoute(SayNameAndAge, "/api/v1/hello/{name:string}/{age:int}");
+
+                await server.RunAsync();
+            }
+        }
+
+        public static Task<HttpResponse> SayHello(RestRequest request, CancellationToken token)
+        {
+            var response = HttpResponse.FromString(
+                "Hello, World!",
+                statusCode: HttpStatusCode.OK);
+
+            return Task.FromResult(response);
+        }
+
+        public static Task<HttpResponse> SayName(RestRequest request, CancellationToken token)
+        {
+            var name = request.RouteInfo.Matches["name"];
+
+            var response = HttpResponse.FromString(
+                $"Hello, {name}!",
+                statusCode: HttpStatusCode.OK);
+
+            return Task.FromResult(response);
+        }
+
+        public static Task<HttpResponse> SayNameAndAge(RestRequest request, CancellationToken token)
+        {
+            var name = request.RouteInfo.Matches["name"];
+            var age = request.RouteInfo.Matches["age"];
+
+            var response = HttpResponse.FromString(
+                $"Hello, {name}, you are {age}!",
+                statusCode: HttpStatusCode.OK);
+
+            return Task.FromResult(response);
+        }
+
+        public static Task<HttpResponse> SayWithQueryString(RestRequest request, CancellationToken token)
+        {
+            var name = request.Context.Request.QueryString.Get("name");
+            var age = request.Context.Request.QueryString.Get("age");
+
+            var response = HttpResponse.FromString(
+                $"Hello, {name ?? "nobody"}, you are {age ?? "a mystery"}!",
+                statusCode: HttpStatusCode.OK);
+
+            return Task.FromResult(response);
+        }
+    }
+}
